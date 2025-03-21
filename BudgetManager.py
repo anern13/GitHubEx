50b8fb669efe73a9bec8a2da41b756d3db8585c6
Ameer Sobih <ameer.sobih@campus.technion.ac.il>
2025-03-21 11:44:07 +0200

    code allahu akbar

--- BudgetManager.py
+++ BudgetManager.py
@@ -1,8 +1,21 @@
 def AddIncome(data):
-    return
+    amount_to_add = input("Enter income you have received \n")
+    description = input("Describe the operation you recently executed \n")
+    data["balance"] = int(data["balance"]) + int(amount_to_add)
+    data["transactions"].append({"type": "income", "amount": amount_to_add, "description": description})
+    print(f"Income of {amount_to_add} added successfully.")
+    return data
 
 def AddExpense(data):
-    return
+    amount_to_subtract = input("Enter expense you have spent \n")
+    description = input("Describe the operation you recently executed \n")
+    if int(amount_to_subtract) > int(data["balance"]):
+        print("Insufficient balance for this expense.")
+    else:
+        data["balance"] = int(data["balance"]) - int(amount_to_subtract)
+        data["transactions"].append({"type": "expense", "amount": amount_to_subtract, "description": description})
+        print(f"Expense of {amount_to_subtract} added successfully.")
+    return data
 
 def ShowBalance(data):
     print(f"Your Current Balance is {data["balance"]}")
